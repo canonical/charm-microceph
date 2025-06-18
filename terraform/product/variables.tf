bedf83881325b27e7e4cc6eb355c0474db19ed33
Amjad <amjad.chami@canonical.com>
2025-06-18 10:25:42 -0400

    add more variables to the microceph module

--- terraform/product/variables.tf
+++ terraform/product/variables.tf
@@ -2,3 +2,27 @@ variable "model" {
   description = "Reference to a `juju_model`."
   type        = string
 }
+
+variable "enable_radosgw" {
+  description = "whether or not to enable radowsgw"
+  type        = bool
+  default     = false
+}
+
+variable "microceph" {
+  description = "configuration for the microceph deployment"
+  type = object({
+    app_name    = optional(string, null)
+    base        = string
+    channel     = string
+    config      = optional(map(string), {})
+    constraints = optional(string, null)
+    resources   = optional(map(string), {})
+    revision    = optional(string, null)
+    units       = optional(number, 3)
+    endpoint_bindings = optional(set(object({
+      space    = string
+      endpoint = optional(string, null)
+    })), {})
+  })
+}
