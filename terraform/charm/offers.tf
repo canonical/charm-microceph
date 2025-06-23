2c7cf4dd371b788956bc27058e9c59d8d49e3d12
Amjad <amjad.chami@canonical.com>
2025-06-23 16:20:00 -0400

    update charm offers

--- terraform/charm/offers.tf
+++ terraform/charm/offers.tf
@@ -1,5 +1,6 @@
 resource "juju_offer" "ceph" {
+  depends_on       = [null_resource.juju_wait]
   model            = var.model
-  application_name = "microceph"
-  endpoints         = ["ceph", "radosgw", "mds"]
+  application_name = var.app_name
+  endpoints        = ["ceph", "radosgw", "mds"]
 }
