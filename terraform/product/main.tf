32c83bdb846ef434df16e8d320d055ec0646d60f
Amjad <amjad.chami@canonical.com>
2025-06-23 16:01:18 -0400

    pass config to charm module

--- terraform/product/main.tf
+++ terraform/product/main.tf
@@ -1,9 +1,14 @@
 
 module "microceph" {
-  source = "../charm"
-
-  model = var.model
-
+  source            = "../charm"
+  app_name          = var.microceph.app_name
+  base              = var.microceph.base
+  channel           = var.microceph.channel
+  config            = var.microceph.config
+  constraints       = var.microceph.constraints
+  endpoint_bindings = var.microceph.endpoint_bindings
+  revision          = var.microceph.revision
+  storage           = var.microceph.storage
+  units             = var.microceph.units
+  model             = var.model
 }
-
-# TODO VAULT MODULE
