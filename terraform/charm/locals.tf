d85e6b0f206bd4f9e188724618b98c852fe49387
Amjad <amjad.chami@canonical.com>
2025-07-03 08:12:59 -0400

    add try blocks and jsondecode

--- terraform/charm/locals.tf
+++ terraform/charm/locals.tf
@@ -1,8 +1,6 @@
 locals {
-  radosgw_user      = data.external.radosgw_user.result
-  access_key        = local.radosgw_user["keys"][0]["user"]
-  secrets_key       = local.radosgw_user["keys"][0]["access_key"]
-
-  endpoint = data.external.s3_endpoints.result["endpoint"]
-
+  radosgw_user = jsondecode(data.external.radosgw_user.result.result)
+  access_key   = try(local.radosgw_user["keys"][0]["user"], "")
+  secrets_key  = try(local.radosgw_user["keys"][0]["access_key"], "")
+  endpoint     = data.external.s3_endpoints.result["endpoint"]
 }
