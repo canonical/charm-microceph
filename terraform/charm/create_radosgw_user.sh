10865496b932bacdc4df08ce01738c3f416e944a
Amjad <amjad.chami@canonical.com>
2025-07-03 10:20:52 -0400

    add missing word

--- terraform/charm/create_radosgw_user.sh
+++ terraform/charm/create_radosgw_user.sh
@@ -3,4 +3,4 @@
 user_id=$1
 display_name=$2
 
-juju ssh microceph/leader -- sudo radosgw-admin user create --uid="${user_id}" --display-name="${display_name}"
+juju ssh microceph/leader -- sudo microceph.radosgw-admin user create --uid="${user_id}" --display-name="${display_name}"
