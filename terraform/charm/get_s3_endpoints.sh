1063c93d4117744d305b8778c81d54c510a01edf
Amjad <amjad.chami@canonical.com>
2025-07-03 09:59:35 -0400

    proper json result from get_s3_endpoint.sh

--- terraform/charm/get_s3_endpoints.sh
+++ terraform/charm/get_s3_endpoints.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -euo pipefail
 APP=$1
-juju status "${APP}"/leader --format=json | jq -r '"{endpoint: \"" + (.applications | .[].units | .[] | .["public-address"] // .address) + "\"}"'
+juju status "${APP}"/leader --format=json | jq -r '"{\"endpoint\": \"" + (.applications | .[].units | .[] | .["public-address"] // .address) + "\"}"'
