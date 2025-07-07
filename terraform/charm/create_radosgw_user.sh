7ab6a6edf93b36e53060bcfbdd63667e2f8ec2bb
Amjad <amjad.chami@canonical.com>
2025-07-07 12:16:58 -0400

    force the output into a string

--- terraform/charm/create_radosgw_user.sh
+++ terraform/charm/create_radosgw_user.sh
@@ -3,4 +3,4 @@
 user_id=$1
 display_name=$2
 
-juju ssh microceph/leader -- sudo microceph.radosgw-admin user create --uid="${user_id}" --display-name="${display_name}"
+juju ssh microceph/leader -- sudo microceph.radosgw-admin user create --uid="${user_id}" --display-name="${display_name}" | jq -r 'to_entries | map({key: .key, value: (.value | tostring)}) | from_entries'
