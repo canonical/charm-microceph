8e8a8334afa2b09fef4aa2aa1ae3fafbab59628e
Amjad <amjad.chami@canonical.com>
2025-07-16 07:03:36 -0400

    in claude i trust

--- terraform/charm/create_radosgw_user.sh
+++ terraform/charm/create_radosgw_user.sh
@@ -4,4 +4,4 @@ user_id=$1
 display_name=$2
 model_name=$3
 
-juju ssh microceph/leader -m "${model_name}" -- "sudo microceph.radosgw-admin user info --uid='${user_id}' --format=json 2>/dev/null || sudo microceph.radosgw-admin user create --uid='${user_id}' --display-name='${display_name}' --format=json" | jq -r 'to_entries | map({key: .key, value: (.value | tostring)}) | from_entries'
+juju ssh microceph/leader -m "${model_name}" -- "sudo microceph.radosgw-admin user info --uid='${user_id}' --format=json 2>/dev/null || sudo microceph.radosgw-admin user create --uid='${user_id}' --display-name='${display_name}' --format=json" | jq -r 'to_entries | map({key: .key, value: (if (.value | type) == "array" or (.value | type) == "object" then .value else (.value | tostring) end)}) | from_entries'
