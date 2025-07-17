43733c07834ca0e34ebae3f59aa1c21009c473ac
Guillaume Boutry <guillaume.boutry@canonical.com>
2025-07-17 12:03:08 +0100

    fix saas typo
    Signed-off-by: Guillaume Boutry <guillaume.boutry@canonical.com>

--- terraform/product/outputs.tf
+++ terraform/product/outputs.tf
@@ -6,7 +6,7 @@ output "charms" {
 
 output "provides" {
   value = {
-    sass      = module.microceph.provides
+    saas      = module.microceph.provides
     endpoints = local.endpoint_bindings
   }
 }
