e03b3909d4c03041a8a649fb6dcb567b7d64176e
Guillaume Boutry <guillaume.boutry@canonical.com>
2025-07-17 09:46:45 +0100

    Fix mis-quoted model
    Signed-off-by: Guillaume Boutry <guillaume.boutry@canonical.com>

--- terraform/charm/get_s3_endpoints.sh
+++ terraform/charm/get_s3_endpoints.sh
@@ -2,4 +2,4 @@
 set -euo pipefail
 APP=$1
 MODEL=$2
-juju status -m "${MODEL} "${APP}"/leader --format=json | jq -r '"{\"endpoint\": \"" + (.applications | .[].units | .[] | .["public-address"] // .address) + "\"}"'
+juju status -m "${MODEL}" "${APP}"/leader --format=json | jq -r '"{\"endpoint\": \"" + (.applications | .[].units | .[] | .["public-address"] // .address) + "\"}"'
