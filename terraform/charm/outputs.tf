f9f0ec1f0cc26f539f0557e1d84b79a7b3f76f32
Amjad <amjad.chami@canonical.com>
2025-07-17 07:08:27 -0400

    access endpoint value directly

--- terraform/charm/outputs.tf
+++ terraform/charm/outputs.tf
@@ -6,7 +6,7 @@ output "provides" {
   value = {
     microceph_endpoints = juju_offer.ceph
     s3 = {
-      endpoint   = data.external.s3_endpoints.result
+      endpoint   = data.external.s3_endpoints.result.endpoint
       access_key = local.access_key
       secret_key = local.secret_key
     }
