efc6f26b1b4cafa9f4879385ec87fbd586e6c022
Utkarsh Bhatt <utkarsh.bhatt@canonical.com>
2025-10-27 23:54:47 +0530

    fix: CI tests
    Signed-off-by: Utkarsh Bhatt <utkarsh.bhatt@canonical.com>

--- src/microceph_remote.py
+++ src/microceph_remote.py
@@ -42,7 +42,6 @@ class RemoteRelationDataKeys(Enum):
     """Keys for the remote relation."""
 
     site_name = "site-name"
-    monitors = "monitors"
     token = "token"
 
 
@@ -58,7 +57,7 @@ class MicrocephRemoteReconcileEvent(RelationEvent):
     pass
 
 
-class MicrocephRemoteUpdateEvent(RelationEvent):
+class MicrocephRemoteUpdateRemoteEvent(RelationEvent):
     """Remote update event for updating remote application."""
 
     pass
@@ -69,7 +68,7 @@ class MicrocephRemoteEvent(ObjectEvents):
 
     microceph_remote_departed = EventSource(MicrocephRemoteDepartedEvent)
     microceph_remote_reconcile = EventSource(MicrocephRemoteReconcileEvent)
-    microceph_remote_update = EventSource(MicrocephRemoteUpdateEvent)
+    microceph_remote_update_remote = EventSource(MicrocephRemoteUpdateRemoteEvent)
 
 
 class MicroCephRemote(Object):
@@ -84,9 +83,9 @@ class MicroCephRemote(Object):
         self.charm = charm
         self.relation_name = relation_name
 
-        self.framework.observe(charm.on[relation_name].joined, self._on_changed)
-        self.framework.observe(charm.on[relation_name].departed, self._on_departed)
-        self.framework.observe(charm.on[relation_name].changed, self._on_changed)
+        self.framework.observe(charm.on[relation_name].relation_joined, self._on_changed)
+        self.framework.observe(charm.on[relation_name].relation_departed, self._on_departed)
+        self.framework.observe(charm.on[relation_name].relation_changed, self._on_changed)
 
         # React to ceph peers to update
         self.framework.observe(charm.on["peers"].relation_departed, self._on_peer_updated)
@@ -102,34 +101,31 @@ class MicroCephRemote(Object):
         # add a check here to go to blocked state if such a relation exists.
 
         # remove remote record
-        self.on.microceph_remote_departed.emit(event)
+        self.on.microceph_remote_departed.emit(event.relation)
 
     def _on_changed(self, event):
         if not self.model.unit.is_leader():
             logger.debug("Not the leader, ignoring remote event")
             return
+        site_name = self.charm.model.config.get("site-name")
 
         with sunbeam_guard.guard(self.charm, self.relation_name):
-            site_name = self.charm.model.config.get("site-name")
-
             if not site_name:
                 event.defer()
                 raise sunbeam_guard.BlockedExceptionError("config site-name not set")
 
-            local_cluster_data = event.relation.data.get(self.charm.app)
-
-            # populate local site name if not added.
-            if not local_cluster_data.get(RemoteRelationDataKeys.site_name, None):
-                local_cluster_data.update({RemoteRelationDataKeys.site_name: site_name})
-
-            local_token = local_cluster_data.get(RemoteRelationDataKeys.token, None)
-            if not local_token:
-                # emit event to update self data in databag.
-                self.on.microceph_remote_update.emit(event)
-
+            local_relation_data = event.relation.data.get(self.charm.app)
             remote_relation_data = event.relation.data.get(event.app)
-            remote_site_name = remote_relation_data.get(RemoteRelationDataKeys.site_name, None)
-            remote_token = remote_relation_data.get(RemoteRelationDataKeys.token, None)
+            local_site_name = local_relation_data.get(str(RemoteRelationDataKeys.site_name), None)
+            local_token = local_relation_data.get(str(RemoteRelationDataKeys.token), None)
+
+            if not local_site_name or not local_token:
+                self.on.microceph_remote_update_remote.emit(event.relation)
+
+            remote_site_name = remote_relation_data.get(
+                str(RemoteRelationDataKeys.site_name), None
+            )
+            remote_token = remote_relation_data.get(str(RemoteRelationDataKeys.token), None)
             if remote_site_name is not None and remote_token is not None:
                 # remote data available, reconcile if necessary
                 self.on.microceph_remote_reconcile.emit(event.relation)
@@ -139,13 +135,12 @@ class MicroCephRemote(Object):
             logger.debug("Not the leader, ignoring remote update event")
             return
 
-        site_name = self.charm.model.config.get("site-name", None)
-
-        if not site_name:
+        if not self.charm.model.config.get("site-name", None):
             # site name is not set
             return
 
-        self.emit.microceph_remote_update.emit(event)
+        for remote_rel in self.charm.model.relations[self.relation_name]:
+            self.on.microceph_remote_update_remote.emit(remote_rel)
 
 
 class MicroCephRemoteHandler(RelationHandler):
@@ -167,7 +162,9 @@ class MicroCephRemoteHandler(RelationHandler):
 
         self.framework.observe(microceph_remote.on.microceph_remote_departed, self._on_departed)
         self.framework.observe(microceph_remote.on.microceph_remote_reconcile, self._on_reconcile)
-        self.framework.observe(microceph_remote.on.microceph_remote_update, self._on_updated)
+        self.framework.observe(
+            microceph_remote.on.microceph_remote_update_remote, self._on_update_remote
+        )
 
         return microceph_remote
 
@@ -178,16 +175,16 @@ class MicroCephRemoteHandler(RelationHandler):
 
     def _on_departed(self, event):
         """Handle integration cleanup."""
-        remote_relation_data = event.relation.data.get(event.app)
-        remote_site_name = remote_relation_data.get(RemoteRelationDataKeys.site_name, None)
+        remote_relation_data = event.relation.data.get(event.relation.app)
+        remote_site_name = remote_relation_data.get(str(RemoteRelationDataKeys.site_name), None)
 
         remove_remote_cluster(remote_site_name)
 
     def _on_reconcile(self, event):
         # fetch remote app data
-        remote_relation_data = event.relation.data.get(event.app)
-        remote_site_name = remote_relation_data.get(RemoteRelationDataKeys.site_name, None)
-        remote_token = remote_relation_data.get(RemoteRelationDataKeys.token, None)
+        remote_relation_data = event.relation.data.get(event.relation.app)
+        remote_site_name = remote_relation_data.get(str(RemoteRelationDataKeys.site_name), None)
+        remote_token = remote_relation_data.get(str(RemoteRelationDataKeys.token), None)
         remote_ceph_monitors = decode_monitors_from_cluster_token(remote_token)
         current_monitors = get_cluster_monitors(remote_site_name)
 
@@ -199,14 +196,14 @@ class MicroCephRemoteHandler(RelationHandler):
                 remote_token=remote_token,
             )
 
-    def _on_updated(self, event):
+    def _on_update_remote(self, event):
         # fetch local app data
-        # TODO: Fix this as event object would not contain the remote relation databag here.
-        local_relation_data = event.relation.data.get(self.charm.app)
-        remote_relation_data = event.relation.data.get(event.app)
 
-        remote_site_name = remote_relation_data.get(RemoteRelationDataKeys.site_name, None)
-        local_token = local_relation_data.get(RemoteRelationDataKeys.token, None)
+        local_relation_data = event.relation.data.get(self.charm.app)
+        remote_relation_data = event.relation.data.get(event.relation.app)
+
+        remote_site_name = remote_relation_data.get(str(RemoteRelationDataKeys.site_name), None)
+        local_token = local_relation_data.get(str(RemoteRelationDataKeys.token), None)
         last_updated_monitors = decode_monitors_from_cluster_token(local_token)
 
         new_token = get_cluster_export_token(remote_site_name)
@@ -214,7 +211,7 @@ class MicroCephRemoteHandler(RelationHandler):
 
         # if monitors differ, update token
         if set(last_updated_monitors) != set(current_monitors):
-            local_relation_data.update({RemoteRelationDataKeys.token: new_token})
+            local_relation_data.update({str(RemoteRelationDataKeys.token): new_token})
 
 
 def decode_monitors_from_cluster_token(token) -> list:
